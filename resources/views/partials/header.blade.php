6b913dff716303ffe9c56bab4b3b8a18367b933e
MdIshtiaque <ishtiaqueferdous109@gmail.com>
2024-06-26 02:20:19 +0600

    Added Avatar in dashboard header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
+<style>
+    .rounded-circle {
+        border-radius: 0 !important;
+    }
+
+    .header-profile-user {
+
+        background-color: transparent !important;
+
+    }
+</style>
 <header id="page-topbar">
     <div class="navbar-header">
         <div class="d-flex">
@@ -25,60 +36,19 @@
             <button type="button" class="btn btn-sm px-3 font-size-16 header-item" id="vertical-menu-btn">
                 <i class="fa fa-fw fa-bars"></i>
             </button>
-
-            <!-- App Search-->
-{{--            <form class="app-search d-none d-lg-block">--}}
-{{--                <div class="position-relative">--}}
-{{--                    <input type="text" class="form-control" placeholder="Search...">--}}
-{{--                    <button class="btn btn-primary" type="button"><i class="bx bx-search-alt align-middle"></i></button>--}}
-{{--                </div>--}}
-{{--            </form>--}}
         </div>
 
         <div class="d-flex">
-
-            <div class="dropdown d-inline-block d-lg-none ms-2">
-                <button type="button" class="btn header-item" id="page-header-search-dropdown"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i data-feather="search" class="icon-lg"></i>
-                </button>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-                     aria-labelledby="page-header-search-dropdown">
-
-                    <form class="p-3">
-                        <div class="form-group m-0">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search ..."
-                                       aria-label="Search Result">
-
-                                <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-{{--            <div class="dropdown d-none d-sm-inline-block">--}}
-{{--                <button type="button" class="btn header-item" id="mode-setting-btn">--}}
-{{--                    <i data-feather="moon" class="icon-lg layout-mode-dark"></i>--}}
-{{--                    <i data-feather="sun" class="icon-lg layout-mode-light"></i>--}}
-{{--                </button>--}}
-{{--            </div>--}}
-
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item bg-light-subtle border-start border-end"
                         id="page-header-user-dropdown"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/avatar.png') }}"
+                         alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1 fw-medium">{{ auth()->user()->name }}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
-                    <!-- item-->
-{{--                    <a class="dropdown-item" href="apps-contacts-profile.html"><i--}}
-{{--                            class="mdi mdi mdi-face-man font-size-16 align-middle me-1"></i> Profile</a>--}}
-{{--                    <a class="dropdown-item" href="auth-lock-screen.html"><i--}}
-{{--                            class="mdi mdi-lock font-size-16 align-middle me-1"></i> Lock Screen</a>--}}
-{{--                    <div class="dropdown-divider"></div>--}}
                     <form action="{{ route('logout') }}" method="post">
                         @csrf
                         <button type="submit" class="dropdown-item" href="auth-logout.html"><i
