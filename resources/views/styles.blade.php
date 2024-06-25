7824658aa41568b8da47ed88c22f72c39a4682a9
MdIshtiaque <ishtiaqueferdous109@gmail.com>
2024-06-26 01:50:29 +0600

    Remove unnecessary Lines

--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -1,16 +1,3 @@
 {{--@vite('resources/css/app.css')--}}
 <link rel="stylesheet" href="{{ asset('build/css/index.css') }}">
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index2.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index3.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index4.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index5.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index6.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index7.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index8.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index9.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index10.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index11.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index12.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index13.css') }}">--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/index14.css') }}">--}}
 @stack('css')
