44d9a2b85342b0195df9ed685e408eedc6f75244
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2024-06-27 18:52:09 +0600

    changed fonts

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,7 +13,8 @@
 <body class="bg-black">
 
 
-    <div class="relative w-full h-[calc(60vh)] lg:min-h-screen bg-cover bg-top mx-auto custom-background max-w-[2536px]">
+    <div
+        class="relative w-full h-[calc(60vh)] lg:min-h-screen bg-cover bg-top lg:bg-top-30 mx-auto custom-background max-w-[2536px]">
         {{-- <div class="absolute top-8 left-8">
             <img class="h-10" src="{{ asset('images/logo.png') }}" alt="company-logo">
         </div> --}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('main')
 @section('content')
     <div
-        class="absolute h-full top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl p-8 pt-0 bg-transparent text-white">
-        <h1 class="title title-bg text-2xl min-w-64 lg:text-5xl mb-4 text-center mx-auto leading-8 py-3 drop-shadow-2xl">
+        class="absolute h-full top-0 left-1/2 transform -translate-x-1/2 w-full max-w-7xl p-8 pt-4 lg:pt-8 bg-transparent text-white">
+        <h1
+            class="title font-sans-medium title-bg text-2xl min-w-64 lg:text-5xl mb-4 text-center mx-auto leading-8 py-3 drop-shadow-2xl">
             <span><svg class="inline-block mr-1 lg:mr-2 h-2 lg:h-5" viewBox="0 0 24 17" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <path
@@ -19,13 +20,14 @@
                         fill="#FFFFFF" />
                 </svg></span>
         </h1>
-        <form class="mt-40 lg:mt-64" action="{{ route('save') }}" method="POST">
+        <form class="mt-40 lg:mt-96" action="{{ route('save') }}" method="POST">
             @csrf
             <div class="w-full lg:max-w-3xl">
-                <h3 class="text-lg lg:text-2xl w-full lg:max-w-3xl">Thank you for choosing Standard Chartered
+                <h3 class="text-xl lg:text-2xl w-full lg:max-w-3xl font-sans-medium">Thank you for choosing Standard
+                    Chartered
                     Priority!
                 </h3>
-                <p class="w-full lg:max-w-3xl mt-4">Please share your name, phone number and city below. A
+                <p class="w-full lg:max-w-3xl mt-4 font-sans-light">Please share your name, phone number and city below. A
                     relationship
                     Standard Chartered
                     will
@@ -41,7 +43,7 @@
                         fill="white" />
                 </svg>
                 <input type="text" name="full_name" placeholder="Full Name"
-                    class="block w-full px-4 py-2 border border-white rounded-full bg-transparent">
+                    class="block w-full px-4 py-2 border border-white rounded-full bg-transparent font-sans-light">
             </div>
 
             <div class="flex flex-col gap-6 lg:flex-row lg:gap-44 items-center lg:justify-between">
@@ -56,20 +58,34 @@
                             fill="white" />
                     </svg>
                     <input type="number" name="mobile" placeholder="Mobile Number"
-                        class="block w-full px-4 py-2 border border-white rounded-full bg-transparent" pattern="\d*">
+                        class="block w-full px-4 py-2 border border-white rounded-full bg-transparent font-sans-light"
+                        pattern="\d*">
                 </div>
 
 
                 <div class="flex w-full items-center gap-3 relative">
-                    <svg class="w-6 h-6 fill-white" width="22" height="31" viewBox="0 0 22 31" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M11 0C4.92731 0 0 4.43506 0 9.9C0 18.7 11 30.8 11 30.8C11 30.8 22 18.7 22 9.9C22 4.43506 17.0727 0 11 0ZM11 15.4C10.1298 15.4 9.27907 15.1419 8.55549 14.6585C7.83192 14.175 7.26796 13.4878 6.93493 12.6838C6.6019 11.8798 6.51477 10.9951 6.68454 10.1416C6.85432 9.28809 7.27338 8.50408 7.88873 7.88873C8.50408 7.27338 9.28809 6.85432 10.1416 6.68454C10.9951 6.51477 11.8798 6.6019 12.6838 6.93493C13.4878 7.26796 14.175 7.83192 14.6585 8.55549C15.1419 9.27907 15.4 10.1298 15.4 11C15.3987 12.1666 14.9347 13.285 14.1099 14.1099C13.285 14.9347 12.1666 15.3987 11 15.4Z"
-                            fill="white" />
+                    <svg class="w-6 h-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.09 39.93">
+                        <defs>
+                            <style>
+                                .cls-1 {
+                                    fill: #fff;
+                                    stroke-width: 0px;
+                                }
+                            </style>
+                        </defs>
+                        <path class="cls-1"
+                            d="m21.55,25.63c.29.25.64.38,1,.38s.71-.13,1-.38c.36-.32,8.87-7.9,8.87-15.76,0-5.44-4.43-9.87-9.87-9.87s-9.87,4.43-9.87,9.87c0,7.86,8.5,15.44,8.87,15.76Zm1-18.34c1.41,0,2.56,1.15,2.56,2.58s-1.15,2.58-2.56,2.58-2.56-1.15-2.56-2.58,1.15-2.58,2.56-2.58Z" />
+                        <path class="cls-1"
+                            d="m40.36,21.62c-.19-.65-.78-1.1-1.45-1.1h-6.21c-2.69,4.33-6.06,7.39-6.67,7.93-.96.85-2.2,1.32-3.48,1.32s-2.52-.47-3.48-1.32c-.61-.54-3.98-3.6-6.67-7.93h-6.21c-.68,0-1.27.45-1.46,1.1l-1.36,4.77,30.45,5.58,7.69-6.34-1.14-4Z" />
+                        <path class="cls-1"
+                            d="m45.04,38l-2.92-10.23-14.49,12.16h15.95c.47,0,.92-.22,1.21-.6.29-.38.38-.87.25-1.33Z" />
+                        <path class="cls-1"
+                            d="m7.41,29.29l-4.62-.85L.06,38c-.13.46-.04.95.25,1.33.29.38.73.6,1.21.6h5.9v-10.64Z" />
+                        <polygon class="cls-1" points="9.53 29.67 9.53 39.93 24.33 39.93 31.71 33.74 9.53 29.67" />
                     </svg>
 
                     <select name="city"
-                        class="block w-full max-w-4xl px-4 py-2 border border-white rounded-full bg-transparent">
+                        class="block w-full max-w-4xl px-4 py-2 border border-white rounded-full bg-transparent font-sans-light">
                         <option class="text-black" value="">Select City</option>
                         <option class="text-black" value="Dhaka">Dhaka</option>
                         <option class="text-black" value="Chattogram">Chattogram</option>
@@ -101,7 +117,7 @@
                 </svg>
                 <h3 class="text-xl">What is in the store for you:</h3>
             </div>
-            <ul class="list-disc mt-5 pl-4">
+            <ul class="list-disc mt-5 pl-4 font-sans-regular">
                 <li>Dedicated Relationship Manager</li>
                 <li>Dedicated Priority Center</li>
                 <li>Access for Airport Lounge</li>
