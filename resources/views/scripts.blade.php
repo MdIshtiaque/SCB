8ec107b87fccf0db1d4e02d13551fd96778cdc77
Dhee Chowdhury <dhee.chowdhury@gmail.com>
2024-06-27 15:33:41 +0600

    initial commit

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,3 +1,3 @@
-{{--@vite('resources/js/main.js')--}}
- <script src="{{ asset('build/js/main.js') }}"></script>
+@vite('resources/js/main.js')
+{{-- <script src="{{ asset('build/js/main.js') }}"></script> --}}
 @stack('js')
