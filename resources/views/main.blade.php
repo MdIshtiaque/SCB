bbd362b4f9b348d08b8918a5807fb8645d077740
dhee-chowdhury <dhee.chowdhury@gmail.com>
2024-06-28 03:05:29 +0600

    added somthing

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,21 +8,23 @@
     <title>Standard Chartered</title>
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
+
+
 </head>
 
 <body class="bg-black">
 
 
-    <div
-        class="relative w-full h-[calc(60vh)] lg:min-h-screen bg-cover bg-top lg:bg-top-30 mx-auto custom-background max-w-[2536px]">
+    <div class="relative w-full h-[calc(60vh)] lg:min-h-screen bg-cover bg-top lg:bg-top-30 mx-auto custom-background max-w-[2536px]">
         {{-- <div class="absolute top-8 left-8">
             <img class="h-10" src="{{ asset('images/logo.png') }}" alt="company-logo">
-        </div> --}}
+    </div> --}}
 
-        @yield('content')
+    @yield('content')
     </div>
 
 </body>
 @include('scripts')
+
 
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/main.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/main.js"],
             refresh: true,
         }),
     ],
+    css: {
+        postcss: {
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
+    },
     build: {
         rollupOptions: {
             output: {
-                entryFileNames: 'js/main.js',       // Output JavaScript as main.js
-                chunkFileNames: 'js/[name].js',     // Name chunks logically
+                entryFileNames: "js/main.js", // Output JavaScript as main.js
+                chunkFileNames: "js/[name].js", // Name chunks logically
                 assetFileNames: ({ name }) => {
-                    if (name.endsWith('.css')) return 'css/index.css'; // Output CSS as index.css
-                    return 'assets/[name].[ext]';  // Output other assets
-                }
-            }
-        }
-    }
+                    if (name.endsWith(".css")) return "css/index.css"; // Output CSS as index.css
+                    return "assets/[name].[ext]"; // Output other assets
+                },
+            },
+        },
+    },
 });
