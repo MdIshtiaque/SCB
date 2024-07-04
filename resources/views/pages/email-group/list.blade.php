a47f5d9a686469a591c4c417c475af4e3b17baad
Md Ishtiaque <ishtiaqueferdous109@gmail.com>
2024-07-04 11:08:45 +0600

    Update

--- resources/views/pages/email-group/list.blade.php
+++ resources/views/pages/email-group/list.blade.php
@@ -26,7 +26,7 @@
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                         <h4 class="mb-sm-0 font-size-18">
-                            Email Recipient List (Email Sent Time :
+                            Email Recipient List (Email Sending Time :
                             {{
                                 Carbon::createFromFormat('H:i', $timer->timer)
                                       ->format('h:i A')
